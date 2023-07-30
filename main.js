2dafcd3e7e196f2d332164641f8e163e72c7a071
Jenseko <jenscode23@gmail.com>
2023-07-30 23:36:31 +0200

    start with JS

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// toggle icon navbar
 
+// scroll sections
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.scroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+  });
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
