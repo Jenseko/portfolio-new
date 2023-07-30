446aa0611195ecbe2517c6fb3d35662965d3de55
Jenseko <jenscode23@gmail.com>
2023-07-30 23:56:53 +0200

    continue with JS

--- main.js
+++ main.js
@@ -11,6 +11,16 @@ window.scroll = () => {
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      // active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
   });
   let header = document.querySelector("header");
 
