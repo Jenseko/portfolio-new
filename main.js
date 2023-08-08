0f7cc58a5dc7d251df9712bf9c2537bc38cca127
Jenseko <jenscode23@gmail.com>
2023-08-08 12:44:52 +0200

    start with scroll animation

--- main.js
+++ main.js
@@ -28,6 +28,12 @@ window.onscroll = () => {
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
+      // active sections for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
     }
   });
 
